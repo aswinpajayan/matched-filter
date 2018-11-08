ea53889b40ee0fc796fbca7af5659b9af50b179a
aswinpajayan <aswinp2008#gmail.com>
2018-11-08 22:47:51 +0530

    removed hard coded value of number of symbols. removed scaling of the pulse at line 66

--- hw6/hw6/matchedFilter.m
+++ hw6/hw6/matchedFilter.m
@@ -21,7 +21,8 @@ clear all;
 	t_trunc = t((2*fs)-idx:(2*fs)+idx);
 	p_trunc = p((2*fs)-idx:(2*fs)+idx);
 	disp(["length of the sequence P_trunc = " num2str(length(p_trunc))])
-%modelling the low pass filter , taking the same sampling freq(fs = 10) and tolerance = 0.01
+%modelling the low pass filter , taking the sampling freq(fs = 10) and tolerance = 0.001
+	tolerance = 0.001
 	RC = 0.5;
 	t = 0:1/fs:3;
 	h = exp(-t/RC);
@@ -34,7 +35,8 @@ clear all;
 %calculating the value of 'N' , given Rb = 0.4 
 	N = fs/0.4;
 %generating random symbols for bpsk modulation
-	sk	= -1 + 2*(rand(1,10000)>0.5);
+	k = 10000; %be the number of arbitrary symbols
+	sk	= -1 + 2*(rand(1,k)>0.5);
 	%sk_1 = zeros(1,2000);
 	%sk_1(1:2:end) = sk;
 
@@ -63,7 +65,7 @@ clear all;
 	variance_param = [0.1 0.2 0.5 0.8 1.0]; 
 	%multiplication by this parameter changes amplitude of the gaussian noise
 	for i = 1:5
-		tx_sig(i,:) = mod_sig_filtered/4+ variance_param(i).*randn(1,len_tx_sig);
+		tx_sig(i,:) = mod_sig_filtered+ variance_param(i).*randn(1,len_tx_sig);
 	endfor
 %matched filter reciever 
 	basis = conv(p_trunc,h_trunc);
@@ -80,10 +82,10 @@ clear all;
 %			title("original transmitted symbols (first 10)");
 %		print -dpng "symbol_detection.png"
 	
-	sk_hat = zeros(5,10001);
+	sk_hat = zeros(5,k);
 	for i = 1:5
 		matched_filter_out = conv(tx_sig(i,:),matched_filter);
-		sk_hat(i,:) = -1 + 2.*(matched_filter_out(2*N:length(p_trunc):end)>0);
+		sk_hat(i,:) = -1 + 2.*(matched_filter_out(2*N:length(p_trunc):end-length(matched_filter))>0);
 	end
 %plotting symbols
 	
@@ -98,7 +100,8 @@ clear all;
 %calculation of bit error rate
 	error_rate = zeros(1,5);
 	for i = 1:5
-		error_rate(i) = sum((sk - sk_hat(i,1:10000)) != 0) / 10000;
+		error_rate(i) = sum((sk - sk_hat(i,1:k)) != 0) / k;
 	end
+	figure;
 	plot(error_rate);
 
